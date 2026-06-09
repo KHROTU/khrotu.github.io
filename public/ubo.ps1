786118a59ff9a2ab765c1409391f14e5fb029717
KHROTU <yanmohan2010@hotmail.com>
2026-06-09 15:33:40 +0800

    fix: idk atp

--- public/ubo.ps1
+++ public/ubo.ps1
@@ -11,8 +11,23 @@ param(
     [string]$UpdateUrl = 'https://ublock.r58playz.dev/update.xml'
 )
 $ErrorActionPreference = 'Stop'
-$ProgressPreference = 'SilentlyContinue'
 $InstallerUrl = 'https://raw.githubusercontent.com/KHROTU/uBlock-mv3-win/master/install.ps1'
+$scriptPath = Join-Path $env:TEMP 'uBOLite-install.ps1'
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-$script = (Invoke-WebRequest -Uri $InstallerUrl -UseBasicParsing).Content
-Invoke-Expression $script
+Write-Host "[*] Fetching $InstallerUrl"
+Invoke-WebRequest -Uri $InstallerUrl -OutFile $scriptPath -UseBasicParsing
+$boundArgs = @()
+foreach ($key in $PSBoundParameters.Keys) {
+    $v = $PSBoundParameters[$key]
+    if ($v -is [switch]) { if ($v) { $boundArgs += "-$key" } }
+    else { $boundArgs += "-$key", ([string]$v) }
+}
+$principal = New-Object Security.Principal.WindowsPrincipal(
+    [Security.Principal.WindowsIdentity]::GetCurrent())
+$isAdmin = $principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+$pwsh = @('-NoProfile', '-ExecutionPolicy', 'Bypass', '-File', $scriptPath) + @($boundArgs)
+if ($isAdmin) {
+    & powershell.exe @pwsh
+} else {
+    Start-Process -FilePath 'powershell.exe' -ArgumentList $pwsh -Verb Runas -Wait
+}
